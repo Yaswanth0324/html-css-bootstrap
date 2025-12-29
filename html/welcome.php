e98f4cb5a0bda4f503e68405a5aaba8fbc5054e3
Yaswanth0324 <yash95632@gmail.com>
2025-12-29 22:43:02 +0530

    addding to github

---
+++ html/welcome.php
@@ -0,0 +1,7 @@
+<<html>
+    <body>
+hi <?php echo $_POST["name"]; ?<br>
+ my fav_language is<?php echo $_POST["fav_language"]; ?<br>
+my 2nd sem subjects are <?php echo $_POST["subjects"]; ?<br>
+
+</html>
